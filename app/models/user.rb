ff2bb888d3a7dafeda8c513ad98e64c0dbd8b31f
baiden-house <57950871+baiden-house@users.noreply.github.com>
2019-11-27 18:39:06 +0900

    committ6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
  has_many :groups, through: members
  has_many :messages
  has_many :members
