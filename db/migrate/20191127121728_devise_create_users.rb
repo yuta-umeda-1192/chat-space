eec718cf1320ccc8d3e2c253975b0ca57b0b2220
baiden-house <57950871+baiden-house@users.noreply.github.com>
2019-11-28 17:00:07 +0900

    n

--- db/migrate/20191127121728_devise_create_users.rb
+++ db/migrate/20191127121728_devise_create_users.rb
@@ -44,4 +44,4 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
-end
+ end
